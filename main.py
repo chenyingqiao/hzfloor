29f724c1441f36ef86b723e82ba92b1e88b302ee
chenyj03 <chenyj03@mingyuanyun.com>
2023-03-08 17:38:42 +0800

    feat: 完成json生成

--- main.py
+++ main.py
@@ -1,76 +1,88 @@
-# coding: utf-8 
-from datetime import datetime
+# coding: utf-8
 import json
 import re
+from datetime import datetime
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+
 
 def sendRobot(content):
     url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=c97d5b18-7d37-4998-a882-5d8ef63f11e9'
     data = {
-                        "msgtype":"text",
-                        "text":{
-                            "content": content
-                        }
-                    }
-    headers = {'Content-Type':'application/json'}
-    requests.post(url=url,headers=headers,data=json.dumps(data))
+        "msgtype": "text",
+        "text": {
+            "content": content
+        }
+    }
+    headers = {'Content-Type': 'application/json'}
+    requests.post(url=url, headers=headers, data=json.dumps(data))
+
 
 def xslpAllLink():
     allLinks = []
     url = 'https://www.dywfdcxy.cn/website/xslp.jsp'
     # 获取页面数量
     page = requests.post(url)
-    searchObj = re.findall(r'共(\d+)页',page.text, re.S)
+    searchObj = re.findall(r'共(\d+)页', page.text, re.S)
     if searchObj:
         totelPageNumber = searchObj[0]
         print("开始解析现售楼盘列表......\n")
         print("共{0}页\n".format(totelPageNumber))
-        for pageNumber in range(1,int(totelPageNumber)):
+        totelPageNumber = 2
+        for pageNumber in range(1, int(totelPageNumber)):
             print("解析第{0}页\n".format(pageNumber))
-            page = requests.post(url,{
-                                    "pageno":pageNumber,
-                                    "projectname": "",
-                                    'compname': "",
-                                    'address': ""
-                                })
-            soup = BeautifulSoup(page.text,"html.parser")
+            page = requests.post(url, {
+                "pageno": pageNumber,
+                "projectname": "",
+                'compname': "",
+                'address': ""
+            })
+            soup = BeautifulSoup(page.text, "html.parser")
             # 找到所有链接
             links = soup.find_all('a')
             for link in links:
                 # 打印链接的文本和链接
                 if "realestate" in link['href']:
-                    allLinks.append("https://www.dywfdcxy.cn/website/{0}".format(link["href"]))
+                    allLinks.append(
+                        "https://www.dywfdcxy.cn/website/{0}".format(link["href"]))
     return allLinks
 
+
 def getProjectPageInfomation(text):
-    soup = BeautifulSoup(text,"html.parser")
+    soup = BeautifulSoup(text, "html.parser")
     tableRow = soup.find_all("tr")
     project_name = ""
     floor = ""
     floor_number = ""
     room_number = ""
     detail_url = ""
+
+    data = []
     for tableRowItem in tableRow:
+        if tableRowItem.find("tr"):
+            continue
         if tableRowItem.find("th") and "项目名称" in tableRowItem.find("th").text:
             project_name = tableRowItem.find("td").text
             print("获取项目信息：{0}".format(project_name))
-        if tableRowItem.has_attr("class") and len(tableRowItem["class"]) >0 and tableRowItem["class"][0] == "Searchboxx":
+        if tableRowItem.has_attr("class") and len(tableRowItem["class"]) > 0 and tableRowItem["class"][0] == "Searchboxx":
             searchboxxRow = tableRowItem.find_all("td")
             floor = searchboxxRow[0].text
             floor_number = searchboxxRow[3].text
             room_number = searchboxxRow[2].text
             if tableRowItem.find("a"):
                 nextPage = tableRowItem.find("a")
-                detail_url = "https://www.dywfdcxy.cn/website/{0}".format(nextPage["href"])
-    return {
-        "project_name": project_name,
-        "floor": floor,
-        "floor_number": floor_number,
-        "room_number": room_number,
-        "detail_url": detail_url,
-    }
+                detail_url = "https://www.dywfdcxy.cn/website/{0}".format(
+                    nextPage["href"])
+            data.append({
+                "project_name": project_name,
+                "floor": floor,
+                "floor_number": floor_number,
+                "room_number": room_number,
+                "detail_url": detail_url,
+            })
+    return data
+
 
 def getALLProjectInfomationAddSaveToDisk(urls):
     data = []
@@ -78,21 +90,20 @@ def getALLProjectInfomationAddSaveToDisk(urls):
     for url in urls:
         page = requests.get(url)
         projectInfomation = getProjectPageInfomation(page.text)
-        data.append(projectInfomation)
-        break
+        data = data + projectInfomation
     return data
+
 
 def getXiaokong(text):
     print("开始获取销控")
     result = []
     data = {}
-    soup = BeautifulSoup(text,"html.parser")
-    allTd = soup.find_all("td",{"width":"180"})
+    soup = BeautifulSoup(text, "html.parser")
+    allTd = soup.find_all("td", {"width": "180"})
     for td in allTd:
-        print(td.text)
-        if not td.find_all("font",{"class":"house_no"}):
+        if not td.find_all("font", {"class": "house_no"}):
             continue
-        houseon = td.find("font",{"class":"house_no"})
+        houseon = td.find("font", {"class": "house_no"})
         state = td.find("img")["state"]
         houseID = td.find("img")["houseid"]
         stateTrans = {
@@ -101,7 +112,7 @@ def getXiaokong(text):
             "cq1": "草签",
             "cq2": "草签",
             "ks": "可售",
-        } 
+        }
         data["title"] = houseon.text
         if state in stateTrans:
             data["state"] = stateTrans[state]
@@ -111,17 +122,17 @@ def getXiaokong(text):
         result.append(data)
     return result
 
+
 def getAllXiaokong(projectData):
-    data = []
     for item in range(len(projectData)):
         page = requests.get(projectData[item]["detail_url"])
-        projectData[item]["xiaokong"]=getXiaokong(page.text)
+        projectData[item]["xiaokong"] = getXiaokong(page.text)
     current_datatime = datetime.now()
-    f = open("./project-{0}{1}{2}.json".format(current_datatime.year,current_datatime.month,current_datatime.day),'w+')
-    f.write(json.dumps(data,ensure_ascii=False))
+    f = open("./project-{0}{1}{2}.json".format(current_datatime.year,
+             current_datatime.month, current_datatime.day), 'w+')
+    f.write(json.dumps(projectData, ensure_ascii=False))
     f.close()
-    return data
-
+    return projectData
 
 
 def readFile(path):
