372fd6a5404f662ee468fcfe1c3bc102386c3837
chenyj03 <chenyj03@mingyuanyun.com>
2023-05-22 21:56:17 +0800

    feat: 修改提示

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ def collectionData(data):
             item = projectCollection[key][fkey]
             if item["number"] == 0:
                 continue
-            content += "[{0}]({1}) {2}\n".format(fkey,item["url"], item["number"])
+            content += "[{0}]({1}) 剩下{2}间房\n".format(fkey,item["url"], item["number"])
             if sys.getsizeof(content) >= 3500:
                 content = "今日{0}年{1}月{2}日楼盘数据\n".format(current_datatime.year,
                                                         current_datatime.month, current_datatime.day) + content
